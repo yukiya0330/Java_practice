040fc10d0c0f0d8f579863ae2ca865589b4cb990
yukiya0330 <kzrktr955@gmail.com>
2022-09-10 08:50:43 +0900

    [Finish]基礎７章

---
+++ JavaWcp/src/question_7_1.java
@@ -0,0 +1,14 @@
+public class question_7_1 {
+	public static void main(String[] args) {
+		int val = 10;
+		if (val <= 5){
+			System.out.println("とても近いです");
+		} else if (val >= 5 && val <= 10) {
+			System.out.println("近いです");
+		} else if (val >= 10 && val <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+}
