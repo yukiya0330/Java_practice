d18541575e7fbcaf211d91b0611c012034404890
yukiya0330 <kzrktr955@gmail.com>
2022-09-09 19:11:57 +0900

    6章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,18 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 10000000000L;
+		double d = 1.5;
+		boolean b = true;
+		Float f = 3.14f;
+		Integer i = 1;
+		Character c = 'あ';
+		int g[] = {1, 2, 3, 4, 5};
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(g[3]);
+	}
+}
