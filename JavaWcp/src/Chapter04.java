04a96ced815811011e04206981e2259aeca878b7
hoshihirayukiya <hoshihirayukiya@localhost>
2022-09-09 15:14:25 +0900

    first commit

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		final int number = 3;
+		String name = "YUKI";
+		System.out.println(name);
+		name = "幸";
+		System.out.println(name);
+		System.out.println(number);
+	}
+}
