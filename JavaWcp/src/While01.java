a2dfb0ac24a29d6e4e495a90b069ab7580c410a3
yukiya <hosiyky33077@gmail.com>
2022-09-10 09:38:51 +0900

    test-push02

--- JavaWcp/src/While01.java
+++ JavaWcp/src/While01.java
@@ -1,6 +1,6 @@
 public class While01 {
     public static void main(String[] args) {
-        int number = 1;
+        int number = 5;
         while (number < 50) {
             number *= 2;
             System.out.println("While01 = " + number);
