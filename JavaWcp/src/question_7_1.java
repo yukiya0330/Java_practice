04b815505532ec3dbf79a813df1968b8708d2f1f
yukiya <ktrkzr955@gmail.com>
2022-09-10 09:23:53 +0900

    test-push

--- JavaWcp/src/question_7_1.java
+++ JavaWcp/src/question_7_1.java
@@ -1,6 +1,6 @@
 public class question_7_1 {
 	public static void main(String[] args) {
-		int val = 15;
+		int val = 20;
 		if (val <= 5){
 			System.out.println("とても近いです");
 		} else if (val >= 5 && val <= 10) {
