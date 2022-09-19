1fbf42c0a2c21f71c4b9be9fe43544b5cac05adc
yukiya <hosiyky33077@gmail.com>
2022-09-19 21:26:17 +0900

    test

--- JavaWcp/src/practice/Soldier.java
+++ JavaWcp/src/practice/Soldier.java
@@ -3,7 +3,7 @@ package practice;
 public class Soldier implements Character {
     // プログラムを作成
 	public int attack() {
-		System.out.println("戦士の会心の一撃！");
+		System.out.println("戦士の会心の一撃！!");
 		return 100;
 	}
 }
